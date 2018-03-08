9d0540c88130912401e77440a643b85cd70e36de
Clic00 <clic00@yahoo.com>
2018-03-08 17:57:48 -0300

    Identificando elementos e programando um script de teste

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -18,7 +18,24 @@ public class InformacoesUsuarioTest {
         navegador.manage().window().maximize();
 
         // Navegando para a página do Taskit!
-        navegador.get("https://www.udemy.com");
+        navegador.get("http://www.juliodelima.com.br/taskit");
+
+        // Clicar no link que possui o texto "Sign in"
+
+        // Clicar no campo com name "login" que está dentro do formulário de id "signinbox"
+
+        // Digitar no campo com o name "login", referido no formulário acima, o texto "julio0001"
+
+        // Clicar no campo com name "password" que está dentro do formulário de id "signinbox"
+
+        // Digitar no campo com o name "password", referido no formulário acima, o texto "123456"
+
+        //Clicar no link com o texto "SIGN IN"
+
+        // Validar que dentro do elemento com class "me" estáo texto "Hi, Julio"
+
+        // Fechar o navegador
+        
 
         // Validação
         assertEquals(1, 1);
